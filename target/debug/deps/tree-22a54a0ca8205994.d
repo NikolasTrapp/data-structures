f5bb0bc9d9ddf712e3748784a25077b728e9c2f3
NikolasTrapp <nikolastrapp@gmail.com>
2023-06-01 15:02:10 -0300

    Criando as funções de adicionar um novo node e de listar todos os nodes

---
+++ target/debug/deps/tree-22a54a0ca8205994.d
@@ -0,0 +1,5 @@
+/home/nikolas/IdeaProjects/tree/target/debug/deps/tree-22a54a0ca8205994: src/main.rs
+
+/home/nikolas/IdeaProjects/tree/target/debug/deps/tree-22a54a0ca8205994.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/tree.d
@@ -0,0 +1,1 @@
+/home/nikolas/IdeaProjects/tree/target/debug/tree: /home/nikolas/IdeaProjects/tree/src/main.rs
